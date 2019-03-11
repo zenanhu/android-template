0444c499c7e871b73404172dbcab61e0bdb177e3
Zenan <zenan2048@gmail.com>
2019-03-12 01:06:04 +0800

    fix gradle error

--- app/src/main/java/info/zenan/android/ui/MainActivity.java
+++ app/src/main/java/info/zenan/android/ui/MainActivity.java
@@ -4,15 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
 import info.zenan.android.AndroidApplication;
 import info.zenan.android.R;
 import info.zenan.android.api.OtherService;
-import info.zenan.android.data.Something;
-import retrofit2.Call;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         OtherService otherService = retrofit.create(OtherService.class);
 
-        Call<List<Something>> repos = otherService.listRepos("zenanhu");
+//        Call<List<Something>> repos = otherService.listRepos("zenanhu");
 
 
 //        otherService.listRepos().subscribeOn(Schedulers.io())
